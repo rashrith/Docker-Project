7056f2b8eaacf90f45511a6860638b5598414fa3
rashrith <48115482+rashrith@users.noreply.github.com>
2024-02-18 19:45:28 -0500

    Update main.py

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ for f in fs:
         fopen=open(path+'/'+f,"r",encoding="utf-8")
         s=(fopen.read().replace("\n"," ")).split()
         total=total+len(s)
-        res=res+"Number of words in file "+str(f)+" is: "+str(len(s))+"\n"
+        res=res+"Total number of words in file "+str(f)+" is: "+str(len(s))+"\n"
         if fopen.name.endswith("IF.txt"):
             st=s
             
@@ -32,10 +32,19 @@ for i in st:
 
 sd= sorted(z.items(), key=lambda x: x[1],reverse=True)
 
-res=res+"Top 3 words and their count:\n"+""+str(sd[0])+'\n'+""+str(sd[1])+'\n'+""+str(sd[2])+'\n'+"Total no of words in both the files : "+str(total)+"\n"+"IP address: "+str(socket.gethostbyname(socket.gethostname())+"\n")
+res=res+"Top 3 words with maximum number of counts :\n"+""+str(sd[0])+'\n'+""+str(sd[1])+'\n'+""+str(sd[2])+'\n'+"Total no of words in both the files : "+str(total)+"\n"+"IP address: "+str(socket.gethostbyname(socket.gethostname())+"\n")
 
+fo=open("/home/data/result.txt","w")
+fo.write(res)
+fo.close()
+fo=open("/home/data/result.txt","r")
+print(fo.read()) 
 fo=open("/home/output/result.txt","w")
 fo.write(res)
 fo.close()
-fo=open("/home/output/result.txt","r")
-print(fo.read()) 
+# path = '/home/output'
+# fs = os.listdir(path)
+# res=""
+# for f in fs:
+# 	res=res+f+'\n'
+# print(fs)
